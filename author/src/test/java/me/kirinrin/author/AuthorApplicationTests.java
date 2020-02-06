a8fd1689384db663554d4e0105ae2e501ad3b9ad
Kirinrin <will.lain@gmail.com>
2020-02-06 17:12:30 +0800

    提交，测试修改提交说明

--- author/src/test/java/me/kirinrin/author/AuthorApplicationTests.java
+++ author/src/test/java/me/kirinrin/author/AuthorApplicationTests.java
@@ -1,13 +1,26 @@
 package me.kirinrin.author;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AuthorApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
 
     @Test
-    void contextLoads() {
+    @WithMockUser(value = "admin", password = "12345")
+    public void contextLoads() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/test")).andDo(MockMvcResultHandlers.log());
     }
 
 }
